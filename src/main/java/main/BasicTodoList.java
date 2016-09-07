8f4ed989a04cb932789b2c4f9cca92c96a644d63
Elarif-ext Ahamada [Caisse Nationale] <ahamada.elarif-ext@le-rsi.fr>
2016-09-07 10:52:15 +0200

    uberjar

--- src/main/java/main/BasicTodoList.java
+++ src/main/java/main/BasicTodoList.java
@@ -1,3 +1,4 @@
+package main;
 import static spark.Spark.delete;
 import static spark.Spark.exception;
 import static spark.Spark.get;
--- src/main/java/main/TodoList.java
+++ src/main/java/main/TodoList.java
@@ -1,3 +1,4 @@
+package main;
 import model.*;
 import spark.*;
 import spark.template.velocity.*;
